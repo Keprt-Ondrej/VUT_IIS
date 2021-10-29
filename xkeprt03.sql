f0d330a60083264dd565127d3371c33266c1b7c9
Ondrej Keprt <you@example.com>
2021-10-29 20:37:07 +0200

    New schema and SQL script for database structure

---
+++ xkeprt03.sql
@@ -0,0 +1,79 @@
+CREATE TABLE `users` (
+  `login` char(8) NOT NULL primary key COMMENT 'xlogin00',
+  `password` varchar(255) NOT NULL,
+  `role` char(1) NOT NULL
+);
+
+CREATE TABLE `subjects` (
+  `subject_ID` varchar(5) primary key NOT NULL,
+  `subject_name` varchar(255) NOT NULL
+);
+
+CREATE TABLE `study` (
+  `login` char(8) NOT NULL,
+  `subject_ID` varchar(5) NOT NULL,
+  `points` int(11) DEFAULT '0',
+  `approveved` tinyint(1) DEFAULT '0',
+  foreign key(login) references users(login),
+  foreign key(subject_ID) references subjects(subject_ID),
+  PRIMARY KEY (`login`,`subject_ID`)
+);
+
+CREATE TABLE `teach` (
+  `login` char(8) NOT NULL,
+  `subject_ID` varchar(5) NOT NULL,
+  `approveved` tinyint(1) DEFAULT '0',
+  foreign key(login) references users(login),
+  foreign key(subject_ID) references subjects(subject_ID),
+  primary key(login,subject_ID)
+);
+
+CREATE TABLE `category` (
+  `category_ID` bigint(20) UNSIGNED NOT NULL primary key AUTO_INCREMENT,
+  `subject_ID` varchar(5),
+  `brief` varchar(255) NOT NULL,
+  foreign key(subject_ID) references subjects(subject_ID)
+);
+
+CREATE TABLE `questions` (
+  `question_ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `category_ID` bigint(20) UNSIGNED NOT NULL,
+  `brief` varchar(50) NOT NULL,
+  `full_question` varchar(1024) NOT NULL,
+  `answer` varchar(1024) DEFAULT NULL,
+  foreign key(category_ID) references category(category_ID),
+  primary key(question_ID,category_ID)
+);
+
+CREATE TABLE `answers` (
+  `login` char(8) NOT NULL,
+  `question_ID` bigint(20) UNSIGNED not NULL,
+  `answer` varchar(1024) DEFAULT NULL,
+  foreign key(question_ID) references questions(question_ID),
+  foreign key(login) references users(login),
+  primary key(login,question_ID)
+);
+
+CREATE TABLE `reactions` (
+  reaction_ID bigint(20) UNSIGNED NOT NULL primary key AUTO_INCREMENT,
+  question_ID bigint(20) UNSIGNED NOT NULL,
+  answer_login char(8) NOT NULL,  
+  reaction_login char(8) NOT NULL,
+  text varchar(1024) NOT NULL,
+  foreign key(question_ID) references questions(question_ID),
+  foreign key(answer_login) references users(login),
+  foreign key(reaction_login) references users(login)
+);
+
+CREATE TABLE `answer_ratings` (
+  question_ID bigint(20) UNSIGNED NOT NULL,
+  answer_login char(8) NOT NULL,
+  rating_login char(8) NOT NULL,
+  foreign key(question_ID) references questions(question_ID),
+  foreign key(answer_login) references users(login),
+  foreign key(rating_login) references users(login),
+  primary key(question_ID,answer_login,rating_login)
+);
+
+INSERT INTO `users` (`login`, `password`, `role`) VALUES
+('admin', 'admin', 'a');
