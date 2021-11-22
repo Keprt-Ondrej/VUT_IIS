75474f943c149782bba7356de6cc750b0cff84e1
EightEnexy <enexyges@gmail.com>
2021-11-23 00:31:40 +0100

    insert user

---
+++ src/app/insert_user.php
@@ -0,0 +1,33 @@
+<?php 
+  session_start();
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+
+  include_once 'database.php';
+
+  $database = new database;
+
+  $db = $database->init();
+  
+  $recv_data = json_decode(file_get_contents('php://input'), true); // POST Data
+
+  $response = array();
+
+  $response["session"] = session_id();
+
+  if($db != null){
+
+    if(isset($_SESSION['role']) && $_SESSION['role']  == 'a'){
+
+      $stmt = $db->prepare('INSERT INTO users (login,password,role)VALUES(login,password,role)');
+
+      if($stmt->execute($recv_data))
+        $response['status'] = 'ok';
+      else
+        $response['status'] = 'login_used';
+
+      echo json_decode($response);
+
+    }
+  }
+?>
