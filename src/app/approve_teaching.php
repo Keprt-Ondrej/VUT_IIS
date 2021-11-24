ff09562730d258de75834f28d678f52d1a46fce8
EightEnexy <enexyges@gmail.com>
2021-11-24 18:47:23 +0100

    approve t

---
+++ src/app/approve_teaching.php
@@ -0,0 +1,42 @@
+<?php 
+  session_start();
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+
+  include_once 'database.php';
+
+  $database = new database;
+
+  $db = $database->init();
+  
+  $recv_data = json_decode(file_get_contents('php://input'), true); // POST Data
+
+  $response = array();
+
+  $response["session"] = session_id();
+
+  if($db != null){
+
+    if(isset($_SESSION['role']) && $_SESSION['role']  == 'm'){
+
+      $stmt = $db->prepare("SELECT role from users where login=?"); 
+      $stmt->execute($recv_data["login"]);
+      $row = $stmt->fetch();
+
+      if($row['role'] != 't'){
+        $response['status'] = 'error';
+        return;
+      }
+
+      $stmt = $db->prepare('UPDATE teach SET approveved=? WHERE login=? ,subject_ID=? ');
+
+      if($stmt->execute($recv_data))
+        $response['status'] = 'ok';
+      else
+        $response['status'] = 'error';
+
+      echo json_decode($response);
+
+    }
+  }
+?>
