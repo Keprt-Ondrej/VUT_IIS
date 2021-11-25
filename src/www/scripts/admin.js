d731ec20faf4e41f05fe1a26f5ace579f6d6e415
KeprtOndrej <keprt.ondrej@gmail.com>
2021-11-25 23:43:21 +0100

    admin list user draft

--- src/www/scripts/admin.js
+++ src/www/scripts/admin.js
@@ -44,3 +44,69 @@ function add_user(){
     }
     return;
 }
+
+function stringify_role(role){
+    switch(role){
+        case "a":
+            return "administrátor";
+        case "m":
+            return "moderátor";
+        case "r": 
+            return "uživatel";
+    }
+}
+
+function show_all_users_content(){
+    try{
+        var request = new XMLHttpRequest();
+        var url = apiURL+"/app/list_users.php";
+        var send_data = JSON.stringify({});
+        request.open("POST", url, true);
+        request.setRequestHeader("Content-Type", "application/json");        
+        request.onreadystatechange = function () {                
+            if (request.readyState === 4 && request.status === 200) {
+                console.log(request.responseText);
+                var received_data = JSON.parse(request.responseText);
+                var destination = document.getElementById("content");
+                destination.innerHTML = `
+                <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
+                <table id="myTable">
+                <tr class="header"><th>Login</th><th>Role</th><th></th><th></th></tr>`
+                var table = document.getElementById("myTable");
+                received_data.forEach(element => {                    
+                    table.innerHTML += `<tr><td>`+ element.login + `</td><td>` + stringify_role(element.password) + `</td><td>button chng pwd</td><td>delete user button</td></tr>`;
+                });
+                destination.innerHTML += "</table>";
+            }
+        }
+        request.send(send_data);
+    }
+    catch(e){
+        alert(e.toString());
+    }
+    return;
+}
+
+
+//*********************************************** */
+function myFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
