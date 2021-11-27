67ed6b7e96948cbe5f1e16b22eeda73533eaa923
EightEnexy <enexyges@gmail.com>
2021-11-27 18:24:42 +0100

    create_category

---
+++ src/app/create_category.php
@@ -0,0 +1,18 @@
+<?php 
+  session_start();
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+
+  include_once 'database.php';
+
+  $database = new Database();
+
+  $recv_data = json_decode(file_get_contents('php://input'), true); // POST Data
+
+    if(isset($_SESSION['role']) && $_SESSION['role'] != 's'){
+
+      $retval = $database->create_category($recv_data);
+      echo json_encode($retval);
+    }
+?>
+  
