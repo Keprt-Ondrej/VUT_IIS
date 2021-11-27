25322c1c50d9a14763fdf6d823e0d196cd0cd69d
KeprtOndrej <keprt.ondrej@gmail.com>
2021-11-27 13:46:12 +0100

    modal window change pwd remove user

--- src/www/scripts/modal_window.js
+++ src/www/scripts/modal_window.js
@@ -1,32 +1,24 @@
-var form = document.getElementById('add-form');
 var modal = document.getElementById('modal');
-
 // open modal button
 var openModal = document.getElementById('open-modal');
-
 // close modal button
 var closeModal = document.getElementById('close-modal');
 
-// First we reset the form to default
-form.reset();
+var modal_body = document.getElementById("modal-body");
+var modal_header = document.getElementById("modal-header");
 
 // Form validation on submit
-form.onsubmit = function(e){
-    e.preventDefault();
-    form.reset();
-    // remove error messages
+function close_modal(){
     modal.style.display = 'none';
-    console.log("zavreno");
+    console.log("schovat modal");
 }
 
-// show modal on click
-openModal.onclick = function() {
+function open_modal(){
     modal.style.display = 'block';
+    console.log("otevrit modal")
 }
 
 // hide modal, remove error messages (if there are any) and reset form when the X was clicked
 closeModal.onclick = function() {
-    form.reset();
-    modal.style.display = 'none';
     modal.style.display = 'none';
 }
