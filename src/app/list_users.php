e1e26f76c433d76819b169e2aaa0898c87c4e949
EightEnexy <enexyges@gmail.com>
2021-11-27 17:29:05 +0100

    nic

--- src/app/list_users.php
+++ src/app/list_users.php
@@ -9,28 +9,29 @@
 
   $recv_data = json_decode(file_get_contents('php://input'), true); // POST Data
 
-    if(isset($_SESSION['role']) && $_SESSION['role']  == 'a'){
+  if(isset($_SESSION['role']) && $_SESSION['role']  == 'a'){
 
-      $retval = $database->list_users($recv_data);
+    $retval = $database->list_users($recv_data);
 
-      if($retval['status'] != 'ok'){
-        echo json_encode($retval);
-        return;
-      }
-
-      $response = array();
-
-      $response['status'] = 'ok';
-
-      $response['users'] = array();
-
-       while($row = $retval['statement']->fetch()){
-            $tmp = new stdClass();
-            $tmp->login  = $row["login"];
-            $tmp->role = $row["role"];
-            array_push($response['users'], $tmp);
-        } 
-      echo json_encode($response);
+    if($retval['status'] != 'ok'){
+      echo json_encode($retval);
+      return;
     }
+
+    $response = array();
+
+    $response['status'] = 'ok';
+
+    $response['users'] = array();
+
+    while($row = $retval['statement']->fetch()){
+      $tmp = new stdClass();
+      $tmp->login  = $row["login"];
+      $tmp->role = $row["role"];
+      array_push($response['users'], $tmp);
+    } 
+      echo json_encode($response);
+  }
+  
 ?>
   
