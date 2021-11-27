925491364f01358618327bdb1e7485e0cad7a551
KeprtOndrej <keprt.ondrej@gmail.com>
2021-11-27 18:14:10 +0100

    approve subject

--- src/www/scripts/moderator.js
+++ src/www/scripts/moderator.js
@@ -11,6 +11,47 @@ function course_list_form_JSON(){
     return JSON.stringify({"unapproved":unapproved,"approved":approved,"undecided":undecided})  
 }
 
+function approved_converter(value){
+    console.log("approved va: "+value);
+    if(value == 1){
+        return "potvrzené";
+    }
+    else if(value === null){
+        return "nerozhodnuto"
+    }
+    else{
+        return "nepotvrzené";
+    }
+    return;   
+}
+
+function approve_course(login,subject_ID,value){
+    console.log("value: "+value);
+    try{
+        var request = new XMLHttpRequest();
+        var url = apiURL+"/app/approve_subject.php";
+        var send_data = JSON.stringify({"approved":value,"login":login,"subject_ID":subject_ID});
+        console.log(send_data);
+        request.open("POST", url, true);
+        request.setRequestHeader("Content-Type", "application/json");        
+        request.onreadystatechange = function () {                
+            if (request.readyState === 4 && request.status === 200) {
+                console.log(request.responseText);
+                var received_data = JSON.parse(request.responseText);
+                var row = document.getElementById(`${received_data.login}_${received_data.subject_ID}_row`);                    
+                row.innerHTML = approved_converter(received_data.approved);          
+            }
+        }
+        request.send(send_data);
+    }
+    catch(e){
+        alert(e.toString());
+    }
+    return;    
+
+}
+
+
 function course_management_list_courses(){
     var send_data = course_list_form_JSON();
     console.log(send_data);
@@ -22,6 +63,22 @@ function course_management_list_courses(){
         request.onreadystatechange = function () {                
             if (request.readyState === 4 && request.status === 200) {
                 console.log(request.responseText);
+                var received_data = JSON.parse(request.responseText);
+                var destination = document.getElementById("content2");
+                destination.innerHTML = `<div>
+                <input type="text" class="myInput" id="myInput" onkeyup="myFunction()" placeholder="Search for names.."></div>
+                <table class="myTable" id="myTable">
+                <tr class="header"><th>Zkratka</th><th>Jméno předmětu</th><th>Vyučující</th><th>Stav</th><th></th><th></th></tr>`
+                var table = document.getElementById("myTable");
+                received_data["subjects"].forEach(element => {
+                    console.log(element);
+                    var status = approved_converter(element.approved);                                                           
+                    table.innerHTML += `<tr><td>${element.subject_ID}</td><td>${element.subject_name}</td><td>${element.login}</td>
+                    <td id="${element.login}_${element.subject_ID}_row">${status}</td>
+                    <td><button type="button" onclick="approve_course(\'${element.login}\',\'${element.subject_ID}\',1);">Potvrdit</button>
+                    </td><td><button type="button" onclick="approve_course(\'${element.login}\',\'${element.subject_ID}\',0);">Zamítnout</button></tr>`;
+                });
+                destination.innerHTML += "</table>";
             }
         }
         request.send(send_data);
