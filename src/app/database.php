43f7a135e42118a9837f6a80d81e8a1de72f276a
KeprtOndrej <keprt.ondrej@gmail.com>
2021-11-28 17:39:30 +0100

    return question_ID  when is created

--- src/app/database.php
+++ src/app/database.php
@@ -387,7 +387,7 @@
           $statement = $this->db->prepare("INSERT INTO questions (category_ID,brief,full_question) VALUES(:category_ID,:brief,:full_question)");
           $statement->execute($args);
 
-          $statement = $this->db->prepare("SELECT question_ID FROM questions WHERE category_ID=:category_ID AND brief:=brief AND full_question=:full_question");
+          $statement = $this->db->prepare("SELECT question_ID FROM questions WHERE category_ID=:category_ID AND brief=:brief AND full_question=:full_question");
           $statement->execute($args);
 
           $response["statement"] = $statement;
