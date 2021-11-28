1344265bd754b629d2e9a78a02af9f2cbbf08770
KeprtOndrej <keprt.ondrej@gmail.com>
2021-11-28 01:07:26 +0100

    approve students

--- src/www/scripts/registered.js
+++ src/www/scripts/registered.js
@@ -76,7 +76,6 @@ function list_subjects(){
     return;    
 }
 
-
 function list_category(subject_ID){
     try{
         var request = new XMLHttpRequest();
@@ -89,14 +88,86 @@ function list_category(subject_ID){
             if (request.readyState === 4 && request.status === 200) {
                 console.log(request.responseText);
                 var content = document.getElementById("content");
-                received_data = JSON.parse(request.responseText);
-                content.innerHTML = "";
+                received_data = JSON.parse(request.responseText); 
                 if(received_data.status == "ok"){
+                    content.innerHTML = "";
                     content.innerHTML += `<h1>${received_data.subject_name}</h1>`;
+                    if(received_data.role == true){
+                        content.innerHTML += `<a href="#" onclick="manage_students(\'${subject_ID}\');">Správa studentů</a>`;
+                    }                    
                     content.innerHTML += `<h3>Kategorie otázek</h3>`;
                     received_data["categories"].forEach(element => { 
                         content.innerHTML+= `<a href="#" onclick="console.log(\'${element.category_ID}\');">${element.brief}</a><br>`
                     });
+                }
+                else{
+                    alert("Chyba");
+                }                
+            }
+        }
+        request.send(send_data);
+    }
+    catch(e){
+        alert(e.toString());
+    }
+    return;    
+}
+
+function approve_student(login,subject_ID,value){
+    console.log("value: "+value);
+    try{
+        var request = new XMLHttpRequest();
+        var url = apiURL+"/app/approve_students.php";
+        var send_data = JSON.stringify({"approved":value,"login":login,"subject_ID":subject_ID});
+        console.log(send_data);
+        request.open("POST", url, true);
+        request.setRequestHeader("Content-Type", "application/json");        
+        request.onreadystatechange = function () {                
+            if (request.readyState === 4 && request.status === 200) {
+                console.log(request.responseText);
+                var received_data = JSON.parse(request.responseText);
+                var row = document.getElementById(`${received_data.login}_row`);                    
+                row.innerHTML = approved_converter(received_data.approved);          
+            }
+        }
+        request.send(send_data);
+    }
+    catch(e){
+        alert(e.toString());
+    }
+    return; 
+}
+
+function manage_students(subject_ID){
+    console.log(subject_ID);
+    try{
+        var request = new XMLHttpRequest();
+        var url = apiURL+"/app/list_students_to_subjects.php";
+        var send_data = JSON.stringify({"subject_ID":subject_ID});
+        console.log("chci kategori: "+send_data);
+        request.open("POST", url, true);
+        request.setRequestHeader("Content-Type", "application/json");        
+        request.onreadystatechange = function () {                
+            if (request.readyState === 4 && request.status === 200) {
+                console.log(request.responseText);
+                var content = document.getElementById("content");
+                received_data = JSON.parse(request.responseText);
+                if(received_data.status == "ok"){
+                    content.innerHTML = "";
+
+                   content.innerHTML += `<div>
+                    <input type="text" class="myInput" id="myInput" onkeyup="myFunction()" placeholder="Search for names.."></div>
+                    <table class="myTable" id="myTable">
+                    <tr class="header"><th>Login</th><th>Stav</th><th></th><th></th></tr>`;
+                    var table = document.getElementById("myTable"); 
+                    var status = approved_converter(received_data.approved);    
+                    received_data["students"].forEach(element => {
+                        table.innerHTML += `<tr><td>${element.login}</td><td id="${element.login}_row">${status}</td>
+                        <td><button type="button" onclick="approve_student(\'${element.login}\',\'${subject_ID}\',1);">Potvrdit</button>
+                        </td><td><button type="button" onclick="approve_student(\'${element.login}\',\'${subject_ID}\',0);">Zamítnout</button></tr>`; 
+                    });
+                    content.innerHTML += `</table>`;
+                    
                 }
                 else{
                     alert("Chyba");
