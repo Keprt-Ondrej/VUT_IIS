a9c79a9d4b643d6cb21e5c26e85464ad487530d5
EightEnexy <enexyges@gmail.com>
2021-11-29 02:21:58 +0100

    quick fix

--- src/app/database.php
+++ src/app/database.php
@@ -266,6 +266,8 @@
             $statement->bindParam(":question_ID", $args["question_ID"]);
             $statement->bindParam(":points", $args["points"]);
 
+            $statement->execute();
+
             /*$statement = $this->db->prepare("UPDATE study SET points=points+(SELECT COUNT(rating_login) FROM answers INNER JOIN answer_ratings ON answers.question_ID=answer_ratings.question_ID AND answers.login=answer_ratings.answer_login) WHERE login=:login AND (SELECT subject_ID FROM category INNER JOIN questions ON questions.category_ID=category.category_ID WHERE question_ID=:question_ID)");
             $statement->bindParam(":login", $args["login"]);
             $statement->bindParam(":question_ID", $args["question_ID"]);
